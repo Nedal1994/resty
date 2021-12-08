6750ffc07d478f132ce9de073aa75661a41332b5
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-12-08 22:49:02 +0200

    ok

--- src/app.js
+++ src/app.js
@@ -11,44 +11,77 @@ import Results from './components/results';
 import { useState } from 'react';
 import axios from 'axios';
 import { useEffect } from 'react';
+import { useReducer } from 'react';
+import History from './components/history/history'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 // API_LINK = https://pokeapi.co/api/v2/pokemon
+// https://swapi.dev/api/people
+// https://fruit-api-301.herokuapp.com/getFruit
 
-export default function App(){
+const initialState=[]
+const reducer = (state=initialState,action)=>
+{
+  let {type,payload} = action
+if(type === 'history')
+{
+  state = [...state,payload]
+    return state
+}
+else
+{
+    return state
+}
+}
+
+const addToHistory = (url,method,results) =>{
+return {
+  type:'history',
+  payload:{url,method,results}
+}
+}
+
+export default function App() {
 
   const [data, setData] = useState(null);
   const [requestParams, setRequestParams] = useState({})
+  const [history, dispatch] = useReducer(reducer, initialState)
 
 
   let callApi = async (requestParams) => {
-    // mock output
+
     const response = await axios.get(requestParams.url)
 
     const data = {
       count: response.data.results.length,
       results: response.data.results,
     };
+  
     setData(data)
     setRequestParams(requestParams)
+    dispatch(addToHistory(requestParams.url,requestParams.method,requestParams.results))
   }
 
-
-  useEffect(()=>
-  {
+  useEffect(() => {
     setRequestParams(requestParams)
   }
-  ,[requestParams])
+    , [requestParams])
+
 
   return (
     <React.Fragment>
       <Header />
+
       <div class='main'>
-      <div>Request Method: {requestParams.method}</div>
-      <div>URL: {requestParams.url}</div>
+        <div>Request Method: {requestParams.method}</div>
+        <div>URL: {requestParams.url}</div>
       </div>
       <Form handleApiCall={callApi} />
 
       <Results data={data} />
+      <History history={history}/>
+      
       <Footer />
     </React.Fragment>
   );
--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './results.scss'
+import Spinner from 'react-bootstrap/Spinner'
 
 export default function Results(data)
 {
   return (
     <section>
-      <pre>{data ? JSON.stringify(data, undefined, 2) : null}</pre>
+      <pre>{data.data ? JSON.stringify(data, undefined, 2) : <Spinner animation="border" role="status">
+  <span className="visually-hidden">Loading...</span>
+</Spinner>}</pre>
     </section>
   );
 }
